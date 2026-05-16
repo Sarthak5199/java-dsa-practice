8075ee2e5d7c07239d30b253c3836ba55cd312b1
Sarthak_jadhav <sarthakjadhav188@gmail.com>
2026-05-16 14:46:02 +0530

    Initial commit - Java DSA practice

--- src/STRINGS/count_vowels_consants.java
+++ src/STRINGS/count_vowels_consants.java
@@ -12,14 +12,18 @@ public class count_vowels_consants {
         int costants=0;
 
         for (int i =0;i<arr.length;i++){
+            if (arr[i]>='a'&&arr[i]<='z'||arr[i]>='A'&&arr[i]<='Z') {
 
-            if(arr[i]=='a'||arr[i]=='i'||arr[i]=='o'||arr[i]=='e'||arr[i]=='u'||arr[i]=='A'||arr[i]=='U'||arr[i]=='I'||arr[i]=='E'||arr[i]=='O'){
-                vowels++;
+
+                if (arr[i] == 'a' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'e' || arr[i] == 'u' || arr[i] == 'A' || arr[i] == 'U' || arr[i] == 'I' || arr[i] == 'E' || arr[i] == 'O') {
+                    vowels++;
+                } else{
+                    costants++;
+                }
             }
-
         }
         System.out.println(vowels);
-       costants=(arr.length)-vowels;
+
         System.out.println(costants);
     }
 }
